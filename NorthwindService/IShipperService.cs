b2abb4c37598deb4980835a2ff3f9733cfcff944
Scien <timothy.brostrom@gmail.com>
2016-01-26 10:46:27 +0100

    Har gjort fram till uppgift 3 på Labb 3
    First commit :dromedary_camel:

---
+++ NorthwindService/IShipperService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace NorthwindService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShipperService" in both code and config file together.
+    [ServiceContract]
+    public interface IShipperService
+    {
+        [OperationContract]
+        Shipper GetShipperByID(int ShipperID);
+
+        [OperationContract]
+        Shipper SaveShipper(int ShipperID, string CompanyName, string Phone);
+
+    }
+    [DataContract]
+    public class Shipper
+    {
+        [DataMember]
+        int ShipperID { get; set; }
+
+        [DataMember]
+        public string CompanyName { get; set; }
+
+        [DataMember]
+        public string Phone { get; set; }
+    }
+}
