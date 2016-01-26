6cf13722f3171a91362a2c8b97e38398f6c8b5ec
Scien <timothy.brostrom@gmail.com>
2016-01-26 14:59:44 +0100

    Gjort Uppgift 4
    :punch: = :dragon:
    !!!!!!!!!!!!!!!!

---
+++ ShipperClient/Index.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ShipperClient.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="FinStil.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Sök Efter Skeppare" />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Spara" />
+    </form>
+</body>
+</html>
---
+++ ShipperClient/Index.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ShipperClient.NwService;
+
+
+namespace ShipperClient
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        private ShipperServiceClient host;
+
+        public Index()
+        {
+            host = new ShipperServiceClient();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox2.Text = host.GetShipperByID(Convert.ToInt32(TextBox1.Text)).ShipperID.ToString();
+            TextBox3.Text = host.GetShipperByID(Convert.ToInt32(TextBox1.Text)).CompanyName;
+            TextBox4.Text = host.GetShipperByID(Convert.ToInt32(TextBox1.Text)).Phone;
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            host.SaveShipper(Convert.ToInt32(TextBox2.Text), TextBox3.Text, TextBox4.Text);
+        }
+    }
+}
